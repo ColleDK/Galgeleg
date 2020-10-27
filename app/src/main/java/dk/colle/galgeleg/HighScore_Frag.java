779075eda6c4651a7eec7a6683136fbc53a27569
ColleDK <s195490@student.dtu.dk>
2020-10-27 13:21:52 +0100

    fix

--- app/src/main/java/dk/colle/galgeleg/HighScore_Frag.java
+++ app/src/main/java/dk/colle/galgeleg/HighScore_Frag.java
@@ -7,7 +7,6 @@ import android.util.ArraySet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
